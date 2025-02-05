e33c762b91ad16d077032f53cf911dd5c3e2482d
Peerapat <peerapas12@gmail.com>
2025-02-05 15:18:02 +0700

    Survey 2nd commit

--- components/course/Survey.vue
+++ components/course/Survey.vue
@@ -1,34 +1,71 @@
 <template>
-      <div class="flex items-center justify-between flex-row">
-        <div class="border border-grey-secondary rounded-xl">
-        <button
-          v-for="button in buttons"
-          :key="button"
-          :class="[
-            'px-6 py-2 rounded-xl',
-            activeButton === button
-              ? 'bg-black-primary text-white'
-              : 'border border-grey-secondary text-base',
-          ]"
-          @click="setActionButton(button)"
-        >
-          {{ button }}
-        </button>
-      </div>
+  <div class="flex items-center justify-between flex-row">
+    <div class="border border-grey-secondary rounded-xl">
+      <button
+        v-for="button in buttons"
+        :key="button"
+        :class="[
+          'px-6 py-2 rounded-xl',
+          activeButton === button
+            ? 'bg-black-primary text-white'
+            : 'border border-grey-secondary text-base',
+        ]"
+        @click="setActionButton(button)"
+      >
+        {{ button }}
+      </button>
     </div>
+    <div class="flex flex-row gap-4">
+      <button
+        v-if="!isComplete"
+        class="flex items-center flex-row justify-center border border-grey-secondary rounded-xl px-4 py-2 gap-2 hover:bg-black-primary hover:text-white"
+      >
+        <Send class="w-5 h-5" />
+        <div class="text-base">Start Survey</div>
+      </button>
+      <button
+        :class="[
+          'flex items-center flex-row justify-center border rounded-xl px-4 py-2 gap-2',
+          isComplete ? 'bg-black-primary text-white' : 'border-grey-secondary',
+        ]"
+        @click="markAsComplete"
+      >
+        <Include class="w-5 h-5" />
+        <div class="text-base">
+          {{ isComplete ? "Complete!" : "Mark as complete" }}
+        </div>
+      </button>
+    </div>
+  </div>
+
+  <div class="rounded-xl p-6 bg-white border border-grey-secondary">
+    <div v-if="activeButton === 'Survey Management'">
+      <SurveyManagement />
+    </div>
+    <div v-else-if="activeButton === 'Survey Statistic'">
+      <SurveyStatistic />
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+import SurveyManagement from "@/components/course/SurveyManagement.vue";
+import SurveyStatistic from "@/components/course/SurveyStatistic.vue";
+import Send from "@/components/icons/Send.vue";
+import Include from "@/components/icons/Include.vue";
 
 const buttons = ["Survey Management", "Survey Statistic"];
-
 const activeButton = ref("Survey Management");
+const isComplete = ref(false);
+
 const setActionButton = (button) => {
   activeButton.value = button;
 };
 
+const markAsComplete = () => {
+  isComplete.value = !isComplete.value;
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- components/icons/Export.vue
+++ components/icons/Export.vue
@@ -9,21 +9,21 @@
     <path
       opacity="0.4"
       d="M5.618 7.79351H4.685C2.65 7.79351 1 9.44351 1 11.4785L1 16.3535C1 18.3875 2.65 20.0375 4.685 20.0375H15.815C17.85 20.0375 19.5 18.3875 19.5 16.3535V11.4685C19.5 9.43951 17.855 7.79351 15.826 7.79351L14.883 7.79351"
-      stroke="black"
+      stroke="currentColor"
       stroke-width="1.5"
       stroke-linecap="round"
       stroke-linejoin="round"
     />
     <path
       d="M10.2499 1V13.041"
-      stroke="black"
+      stroke="currentColor"
       stroke-width="1.5"
       stroke-linecap="round"
       stroke-linejoin="round"
     />
     <path
       d="M7.33478 3.9283L10.2498 1.0003L13.1658 3.9283"
-      stroke="black"
+      stroke="currentColor"
       stroke-width="1.5"
       stroke-linecap="round"
       stroke-linejoin="round"
