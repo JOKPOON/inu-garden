8e5e038992f092887ba6eabd29a7c6a2139233c7
Peerapat Padtawaro <peerapas12@gmail.com>
2025-02-19 14:18:56 +0700

    Edit-Popup

--- components/popups/StatusPopup.vue
+++ components/popups/StatusPopup.vue
@@ -1,56 +1,66 @@
 <template>
   <teleport to="body">
     <div
-      v-if="status === 'success'"
-      class="w-full h-full fixed inset-0 flex items-center justify-center bg-black-primary bg-opacity-50 z-50"
+      class="w-full h-full fixed inset-0 flex items-center justify-center bg-black-primary bg-opacity-20 z-50"
     >
-      <div class="bg-white p-6 rounded-2xl shadow-lg">
-        <h2
-          class="text-lg font-semibold mb-4 w-full text-center pb-2 border-b px-6"
+      <div
+        v-if="status === 'success'"
+        class="h-auto fixed bottom-8 right-8 flex items-center justify-center z-50"
+      >
+        <div
+          class="bg-white p-4 rounded-2xl shadow-xl border border-grey-tertiary"
         >
-          {{ message }}
-        </h2>
-        <div class="flex items-center justify-center flex-col">
-          <img
-            :src="Success"
-            alt="Banner Login"
-            class="w-36 max-w-md object-cover rounded-2xl mb-4"
-          />
-        </div>
-        <div class="flex gap-4">
-          <button
-            @click="closeButton"
-            class="w-full font-medium py-3 bg-gray-300 rounded-xl hover:bg-black-primary hover:text-white"
-          >
-            Confirm
-          </button>
+          <div class="flex flex-row h-full gap-4">
+            <div class="h-14 w-[6px] text-green-600 rounded-3xl"></div>
+            <div class="flex flex-col">
+              <h2
+                class="text-lg font-semibold w-full text-start text-green-600"
+              >
+                Error!
+              </h2>
+              <div class="flex items-center justify-center flex-col">
+                {{ message }}
+              </div>
+              <div class="flex gap-2"></div>
+            </div>
+            <div class="flex flex-start">
+              <button
+                @click="closeButton"
+                class="text-grey-primary font-semibold hover:text-black-primary flex h-6"
+              >
+                X
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div
-      v-else
-      class="w-full h-full fixed inset-0 flex items-center justify-center bg-black-primary bg-opacity-50 z-50"
-    >
-      <div class="bg-white p-6 rounded-2xl shadow-lg">
-        <h2
-          class="text-lg font-semibold mb-4 w-full text-center pb-2 border-b px-6 text-red-600"
+      <div
+        v-else
+        class="h-auto fixed bottom-8 right-8 flex items-center justify-center z-50"
+      >
+        <div
+          class="bg-white p-4 rounded-2xl shadow-xl border border-grey-tertiary"
         >
-          {{ message }}
-        </h2>
-        <div class="flex items-center justify-center flex-col px-16">
-          <img
-            :src="Unsuccess"
-            alt="Banner Login"
-            class="w-36 max-w-md object-cover rounded-2xl mb-4 p-2"
-          />
-        </div>
-        <div class="flex gap-4">
-          <button
-            @click="closeButton"
-            class="w-full font-medium py-3 bg-gray-300 rounded-xl "
-          >hover:bg-black-primary hover:text-white
-            Confirm
-          </button>
+          <div class="flex flex-row h-full gap-4">
+            <div class="h-14 w-[6px] bg-red-600 rounded-3xl"></div>
+            <div class="flex flex-col">
+              <h2 class="text-lg font-semibold w-full text-start text-red-600">
+                Error!
+              </h2>
+              <div class="flex items-center justify-center flex-col">
+                {{ message }}
+              </div>
+              <div class="flex gap-2"></div>
+            </div>
+            <div class="flex flex-start">
+              <button
+                @click="closeButton"
+                class="text-grey-primary font-semibold hover:text-black-primary flex h-6"
+              >
+                X
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -60,8 +70,6 @@
 <script setup>
 import { defineProps, defineEmits } from "vue";
 import { useRouter } from "vue-router";
-import Success from "@/components/images/Success.png";
-import Unsuccess from "@/components/images/Unsuccess.jpg";
 
 const props = defineProps({
   status: {
@@ -74,7 +82,7 @@ const props = defineProps({
   },
   path: {
     type: String,
-    required: true, 
+    required: true,
   },
 });
 const emit = defineEmits(["close"]);
