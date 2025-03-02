ad9d17ee66ddd3dd00e7bd5a47c208d04beaa0cf
Peerapat Padtawaro <peerapat.p@houseofdev.tech>
2025-03-02 21:19:44 +0700

    Add student modal

---
+++ pages/test.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex flex-col gap-4">
+      <div class="flex flex-row">
+        <CourseNav :currentRouteName="'Enrollments'" />
+      </div>
+      <Enrollments />
+    </div>
+  </template>
+  
+  <script setup>
+  import { useI18n } from "vue-i18n";
+  import CourseNav from "@/components/course/CourseNav.vue";
+  import Enrollments from "@/components/course/Enrollments.vue";
+  
+  const { t } = useI18n();
+  
+  useHead({
+    title: "Inu - Courses Enrollments",
+    description: t("seo.desc"),
+  });
+  
+  definePageMeta({
+    layout: "landing",
+  });
+  </script>
+  
+  <style lang="scss" scoped></style>
+  
